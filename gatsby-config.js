f2d01a144bf48e5a4fcee27f9ec69099f62f91eb
Taufan Rezzafri <rezzafri@gmail.com>
2023-03-20 15:33:08 +0700

    Update manifest.

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Website Ikatan APoteker Indonesia Kabupaten `,
+        short_name: `iaikabupatenblitar`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#e36955`,
+        theme_color: `#e36955`,
+        display: `browser`,
+        icon: `src/images/logo-iai-blitar.png`, // This path is relative to the root of the site.
       },
     },
     {
