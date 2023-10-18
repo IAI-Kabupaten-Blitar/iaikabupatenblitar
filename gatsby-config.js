130d76cf3d2180ab5dc15277b344cfdb0b360ae3
Taufan Rezzafri <rezzafri@gmail.com>
2023-10-18 20:22:56 +0700

    Add decapcms.

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     author: `@rezzafri_apt`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-image`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
