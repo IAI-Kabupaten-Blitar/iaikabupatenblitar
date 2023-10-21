f0b33826fc3e187b4a01036dfee5e9111ac8e69e
Taufan Rezzafri <rezzafri@gmail.com>
2023-10-21 15:18:01 +0700

    Move config to js, editorial_workflow for production.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,7 @@
 import CMS from "decap-cms-app";
 import slugify from "@sindresorhus/slugify";
 import id from "./id";
+import postsCollection from "./postsCollection";
 
 CMS.registerLocale("id", id);
 
@@ -9,8 +10,21 @@ const config = {
     name: "git-gateway",
     branch: process.env.GATSBY_BRANCH || "master",
   },
-  local_backend: process.env.GATSBY_LOCAL_BACKEND === "true",
+  local_backend: process.env.NODE_ENV === "development",
+  locale: "id",
+  media_folder: "static/images/uploads",
+  public_folder: "/images/uploads",
+  collections: [postsCollection],
 };
+
+if (process.env.NODE_ENV === "production") {
+  config.publish_mode = "editorial_workflow";
+  const postsIndex = config.collections.findIndex(
+    collection => collection.name === "posts"
+  );
+  if (postsIndex !== -1)
+    config.collections[postsIndex].preview_path = "berita/{{slug}}";
+}
 
 CMS.registerEventListener({
   name: "preSave",
---
+++ src/cms/postsCollection.js
@@ -0,0 +1,54 @@
+const postsCollection = {
+  name: "posts",
+  label: "Berita",
+  folder: "src/contents/posts",
+  create: true,
+  media_folder: "",
+  public_folder: "",
+  path: "{{year}}-{{month}}-{{day}}-{{slug}}/index",
+  editor: {
+    preview: false,
+  },
+  fields: [
+    {
+      label: "Judul",
+      name: "title",
+      widget: "string",
+      required: true,
+    },
+    {
+      label: "Slug",
+      name: "slug",
+      widget: "string",
+      required: false,
+      hint: "Biarkan kosong untuk menghasilkan slug secara otomatis",
+      pattern: ["^[-a-z0-9]+$", "Must be a valid URL slug"],
+    },
+    {
+      label: "Tanggal",
+      name: "date",
+      widget: "datetime",
+      format: "YYYY-MM-DD",
+      date_format: true,
+      time_format: false,
+      required: true,
+    },
+    {
+      label: "Penulis",
+      name: "author",
+      widget: "string",
+    },
+    {
+      label: "Gambar Utama",
+      name: "thumbnail",
+      widget: "image",
+    },
+    {
+      label: "Tulisan",
+      name: "body",
+      widget: "markdown",
+    },
+  ],
+};
+
+export default postsCollection;
